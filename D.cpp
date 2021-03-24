a7badf1ddbe055f1523531a37c0cde93ab9ad031
kaisersir <ivan.gazzi.marcal@gmail.com>
2021-03-24 13:19:44 -0300

    criada Classe D

---
+++ D.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class D{
+    private:
+        int d1;
+        int d2;
+    
+    
+    public:
+        void MD1(){
+            cout<<"metodo 1"<<endl;
+        }
+        void MD2(){
+            cout<<"metodo 2"<<endl;
+        }
+
+}
+
+int main (){
+    
+}
