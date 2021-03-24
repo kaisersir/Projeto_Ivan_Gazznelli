316ea5fc17ac495e43600cdcfd9e041a60e0fcea
Ivan Gazzinelli <39752863+kaisersir@users.noreply.github.com>
2021-03-24 12:45:47 -0300

    Create B.cpp

---
+++ B.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class B{
+    private:
+        int B1;
+        float B2;
+    
+    
+    public:
+        void MB1(){
+            cout<<"MB1"<<endl;
+        }
+        void MB2(){
+            cout<<"MB2"<<endl;
+        }
+}
+
+int main (){
+    
+}
