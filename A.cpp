a33e92156bfdb70375d6867b6f6c866f08abaa16
Ivan Gazzinelli <39752863+kaisersir@users.noreply.github.com>
2021-03-24 12:45:29 -0300

    Create A.cpp

---
+++ A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class A{
+    private:
+        int A1;
+        float A2;
+    
+    
+    public:
+        void MA1(){
+            cout<<"MA1"<<endl;
+        }
+        void MA2(){
+            cout<<"MA2"<<endl;
+        }
+}
+
+int main (){
+    
+}
