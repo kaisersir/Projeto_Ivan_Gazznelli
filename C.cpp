82a33d555e95e5101174eced1b6a00308e85ee76
kaisersir <ivan.gazzi.marcal@gmail.com>
2021-03-24 13:06:09 -0300

    criacao classe C

---
+++ C.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class C{
+    private:
+        char C1;
+        float c2;
+    
+    
+    public:
+        void MC1(){
+            cout<<"MC1"<<endl;
+        }
+        void MC2(){
+            cout<<"MC2"<<endl;
+        }
+}
+
+int main (){
+    
+}
